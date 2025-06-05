8091d73cc13451da82c35247cb0801b1d67dfcd3
Samad Kausar <samad.kausar@w3villa.com>
2025-06-05 17:29:22 +0530

    active

--- config/initializers/active_record_string_limit.rb
+++ config/initializers/active_record_string_limit.rb
@@ -1,6 +1,12 @@
 ActiveSupport.on_load :active_record do
-  ActiveRecord::Base.connection.execute("SET GLOBAL innodb_strict_mode=0;")
-  ActiveRecord::Base.connection.execute("SET GLOBAL sql_mode='NO_ENGINE_SUBSTITUTION';")
+  begin
+    # Set session-level variables instead of global
+    ActiveRecord::Base.connection.execute("SET SESSION innodb_strict_mode=0;")
+    ActiveRecord::Base.connection.execute("SET SESSION sql_mode='NO_ENGINE_SUBSTITUTION';")
+  rescue ActiveRecord::StatementInvalid => e
+    # Log the error but don't fail if we can't set these variables
+    Rails.logger.warn "Unable to set MySQL session variables: #{e.message}"
+  end
   
   module ActiveRecord
     module ConnectionAdapters
