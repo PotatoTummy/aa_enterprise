9cd6017095891e1c325b75f573798e9bcf66a1b7
Samad Kausar <samad.kausar@w3villa.com>
2025-06-11 16:21:13 +0530

    changed render config

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+# Install MySQL client
+curl -LO https://dev.mysql.com/get/mysql-apt-config_0.8.24-1_all.deb
+dpkg -i mysql-apt-config_0.8.24-1_all.deb
+apt-get update
+apt-get install -y mysql-client
+
 bundle install --deployment
 bin/rails assets:precompile
 bin/rails assets:clean
