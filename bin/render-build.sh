37a4a2fc670d4cfb58816a3afa504f65aa2bbf77
Samad Kausar <samad.kausar@w3villa.com>
2025-06-11 17:25:46 +0530

    claude

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,11 +3,10 @@
 set -o errexit
 
 # Install MySQL client
-curl -LO https://dev.mysql.com/get/mysql-apt-config_0.8.24-1_all.deb
-dpkg -i mysql-apt-config_0.8.24-1_all.deb
 apt-get update
 apt-get install -y mysql-client
 
-bundle install --deployment
-bin/rails assets:precompile
-bin/rails assets:clean
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
